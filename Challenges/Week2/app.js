b86e8a39ae32624591732997446f259f89272cbb
Jennifer Varahala <jenvara@outlook.com>
2016-10-05 17:36:40 -0400

    Updated Challenges/Solutions from CodeLouisville

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,11 +1,8 @@
 //create function
   //control and return statements
-//Prompt user for input and store variable
-//Call function
-//Alert user results
 
-var hello = prompt("What is another word for hi?");
-  while(hello !== "hello") {
-	  hello = prompt("Invalid Answer. What is another word for hi?");
-  }
-document.write("Hello World!");
+//Prompt user for input and store variable
+
+//Call function
+
+//Alert user results
